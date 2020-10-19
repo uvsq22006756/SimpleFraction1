f182b594605829a757af1a1e8d41fe78c4d67c73
LANOUNI <user@debian.arcahe.ovh>
2020-10-19 16:56:46 +0200

    Premier Commit

---
+++ SimpleFraction1.java
@@ -0,0 +1,15 @@
+public class SimpleFraction1{
+    int denominateur;
+    int numerateur;
+    
+    public SimpleFraction1(int denominateur, int numerateur){
+        this.denominateur ;
+        this.numerateur;
+    }
+
+    public String toString(){
+        return "la fraction a pour " + numerateur + " et pour denominateur" + denominateur;
+    }
+
+
+}
